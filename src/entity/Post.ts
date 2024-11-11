f5a168db592e8e263567814674757566538352a5
berbraga <bernardobraga75@gmail.com>
2024-11-11 19:52:18 -0300

    berbraga: entidades post e user

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
 
-//TODO Crie a entidade de Post
+@Entity("post")
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  title!: string;
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  description!: string;
+
+  @Column({ type: "int", nullable: false })
+  userId!: number;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  user!: User;
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
-//TODO Crie a entidade de User
+@Entity("user")
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number; // Adicionando o operador "!" para sinalizar inicialização pelo TypeORM
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  firstName!: string;
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  lastName!: string;
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  email!: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts!: Post[];
+}
